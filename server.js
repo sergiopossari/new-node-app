e54e716240ae3e8e8454b951c90fa9846bf724a8
Sergio Possari <sergio.possari@gmail.com>
2019-06-27 20:34:42 -0300

    complemento

--- server.js
+++ server.js
@@ -25,9 +25,12 @@
  const express = require('express');
  const app = express();
 
- app.listen(3000, function () {
-   console.log('server running on port 3000');   
- })
+ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+  
+ app.listen(server_port, server_ip_address, function () {
+   console.log( "Listening on " + server_ip_address + ", port " + server_port )
+ });
 
  app.get('/', (req, res) => {
     res.send('Hello world!')
